37930beb0145624fe9b2698c789ccb91a25d9dce
Samir Rojas <samirrojasgamonal@gmail.com>
2026-07-05 20:22:14 -0500

    -Se corrige app.config,ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
